8af4875f0d3b3ee236629b331fb4dcec1ddedbb3
ggichuru <ggichuru.dev@gmail.com>
2021-02-19 01:16:03 +0300

    add: New Screen task: Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'LearnMe',
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text('Learning Flutter'),
-          ),
-        ),
         body: Center(
           child: RandomWords(),
         ),
@@ -75,9 +70,43 @@ class _RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Center(
+          child: Text('Name Generator'),
+        ),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided,),
+          );
+        },
+      )
     );
   }
 }
